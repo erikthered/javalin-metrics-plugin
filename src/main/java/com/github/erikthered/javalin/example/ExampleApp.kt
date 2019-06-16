2728c49d8729ee7f3fca4f3ae2f6caac753d661c
Erik Nelson <erik.david.nelson@gmail.com>
2019-06-16 10:31:25 -0400

    Rename the example app and move into appropriate package

---
+++ src/main/java/com/github/erikthered/javalin/example/ExampleApp.kt
@@ -0,0 +1,16 @@
+package com.github.erikthered.javalin.example
+
+import com.github.erikthered.javalin.plugin.MetricsPlugin
+import io.javalin.Javalin
+
+fun main() {
+    Javalin.create()
+            .register(MetricsPlugin())
+            .get("/hello") { ctx ->
+                ctx.result("Hello world")
+            }
+            .get("/goodbye") { ctx ->
+                ctx.result("Goodbye")
+            }
+            .start(7000)
+}
