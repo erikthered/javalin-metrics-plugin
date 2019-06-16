a7a759858e3d7ad2973cde7c44777ab23f4d6c1e
Erik Nelson <erik.david.nelson@gmail.com>
2019-06-16 08:43:31 -0400

    Add some basic testing around plugin behavior
    I'm still getting a handle on the testing facilities for
    Javalin, in the meantime doing a live test (launching the server
    and executing real requests) was the easiest way to verify the
    desired behavior was happening.

--- src/test/java/com/github/erikthered/javalin/plugin/MetricsPluginTest.java
+++ src/test/java/com/github/erikthered/javalin/plugin/MetricsPluginTest.java
@@ -1,5 +1,45 @@
 package com.github.erikthered.javalin.plugin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.javalin.Javalin;
+import org.eclipse.jetty.client.HttpClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MetricsPluginTest {
 
+  private Javalin javalin;
+
+  @Before
+  public void setup() {
+    javalin = Javalin.create()
+        .register(new MetricsPlugin())
+        .get("/test/1", ctx -> ctx.result("Test response 1"))
+        .get("/test/2", ctx -> ctx.result("Test response 2"))
+        .get("/test/3", ctx -> ctx.result("Test response 3"))
+        .start(7777);
+  }
+
+  @After
+  public void teardown() {
+    javalin.stop();
+  }
+
+  @Test
+  public void testRequestsAreCountedProperly() throws Exception {
+    HttpClient client = new HttpClient();
+    client.start();
+    client.GET("http://localhost:7777/test/1").getStatus();
+    client.GET("http://localhost:7777/test/2").getStatus();
+    client.GET("http://localhost:7777/test/3").getStatus();
+    assertThat(MetricsRegistry.STATS.size()).isEqualTo(3);
+    client.stop();
+  }
+
+  @Test
+  public void testMetricsRequestsAreNotCounted() {
+
+  }
 }
