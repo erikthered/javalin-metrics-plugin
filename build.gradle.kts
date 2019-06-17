4d5dd732f1630e23d309f4a237c3e3e9bc3c91a9
Erik Nelson <erik.david.nelson@gmail.com>
2019-06-16 21:39:05 -0400

    Cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
-
-    // Apply the application plugin to add support for building a CLI application.
-    application
 }
 
 repositories {
-    // Use jcenter for resolving your dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
 }
 
@@ -22,9 +16,4 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation("org.assertj:assertj-core:3.12.2")
-}
-
-application {
-    // Define the main class for the application.
-    mainClassName = "com.github.erikthered.javalin.example.ExampleAppKt"
 }
